d2a22754fb275517d7c9f616fadcd50d681f86fb
BuildTools <unconfigured@null.spigotmc.org>
2024-04-12 19:07:57 +0200

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
 import './App.css';
+import { jwtDecode } from 'jwt-decode';
+import React, { useState } from 'react';
 
-function App() {
+const TokenStorage = () => {
+  const [token, setToken] = useState('');
+
+  const setJWTToken = (newToken) => {
+    setToken(newToken);
+  };
+
+  const clearJWTToken = () => {
+    setToken('');
+  };
+
+  return {
+    token,
+    setJWTToken,
+    clearJWTToken,
+  };
+};
+
+const Login = ({ tokenStorage }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/account/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const token = await response.text(); // Odczytujemy token jako tekst
+        tokenStorage.setJWTToken(token);
+
+        // Dekodowanie tokena po ustawieniu
+        const decoded = jwtDecode(token);
+        console.log(decoded);
+      } else {
+        console.error('Błąd logowania');
+      }
+    } catch (error) {
+      console.error('Wystąpił błąd', error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleLogin}>Login</button>
     </div>
   );
-}
+};
+
+const App = () => {
+  const tokenStorage = TokenStorage();
+
+  return (
+    <div>
+      <h1>Logowanie</h1>
+      <Login tokenStorage={tokenStorage} />
+    </div>
+  );
+};
 
 export default App;
